bc11c413f39e68f5248a8b9c3b561cae221cc1fd
Mitja Sainio <msainio@protonmail.com>
2024-12-21 13:39:44 +0200

    Update scraping

--- src/scrape_articles.py
+++ src/scrape_articles.py
@@ -3,13 +3,14 @@
 import requests
 from tqdm import tqdm
 
+
 def main():
     url_template = "https://yle.fi/a/74-{idx}"
-    file_template = "data/yle2024/74-{idx}.html"
+    file_name_template = "data/yle2024/74-{idx}.html"
 
-    first_2024 = 20067251
-    most_recent = 20132993  # Fri 20 Dec 17:52:50 EET 2024 
-    indices = range(first_2024, most_recent + 1)
+    idx_first_2024 = 20067251
+    idx_most_recent = 20132993  # Fri 20 Dec 17:52:50 EET 2024 
+    indices = range(idx_first_2024, idx_most_recent + 1)
 
     with requests.Session() as session:
         for idx in tqdm(indices):
@@ -17,10 +18,11 @@ def main():
             with session.get(url, stream=True) as r:
                 if r.status_code != 200:
                     continue
-                filename = file_template.format(idx=idx)
-                with open(filename, "wb") as fd:
+                file_name = file_name_template.format(idx=idx)
+                with open(file_name, "wb") as file:
                     for chunk in r.iter_content(chunk_size=128):
-                        fd.write(chunk)
+                        file.write(chunk)
+
 
 if __name__ == "__main__":
     main()
