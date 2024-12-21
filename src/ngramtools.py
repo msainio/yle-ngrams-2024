12d2c95d7f43ac4dd400dcfaa81fed3595682dd9
Mitja Sainio <msainio@protonmail.com>
2024-12-21 13:42:15 +0200

    Fix indentation

--- src/ngramtools.py
+++ src/ngramtools.py
@@ -8,17 +8,17 @@ class NgramGenerator:
         self.finders = {}
 
     def update(self, words, language):
-    finder_seq = TrigramCollocationFinder.from_words(words)
+        finder_seq = TrigramCollocationFinder.from_words(words)
 
-    if language not in self.finders.keys():
-        self.finders[language] = finder_seq
-    else:
-        finder_all = self.finders[language]
-        for key, value in vars(finder_seq).items():
-            if key != "N":
-                fd = getattr(finder_all, key)
-                setattr(finder_all, key, fd.update(value))
-        finder_all.N = finder_all.word_fd.N()
+        if language not in self.finders.keys():
+            self.finders[language] = finder_seq
+        else:
+            finder_all = self.finders[language]
+            for key, value in vars(finder_seq).items():
+                if key != "N":
+                    fd = getattr(finder_all, key)
+                    setattr(finder_all, key, fd.update(value))
+            finder_all.N = finder_all.word_fd.N()
 
     def serialize(self):
         return {k: serialize_finder(v) for k, v in self.finders.items()}
