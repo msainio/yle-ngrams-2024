24eb8551f4c2f2420e3a489759d7a26b9d7d2cfa
Mitja Sainio <msainio@protonmail.com>
2024-12-22 13:24:57 +0200

    Ensure output dir exists

--- src/scrape_articles.py
+++ src/scrape_articles.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
+import os
 import requests
 from tqdm import tqdm
 
 
 def main():
+    output_dir = Path("data/yle2024")
+    os.makedirs(output_dir, exist_ok=True)
+
     url_template = "https://yle.fi/a/74-{idx}"
-    file_name_template = "data/yle2024/74-{idx}.html"
+    filename_template = "74-{idx}.html"
 
     idx_first_2024 = 20067251
     idx_most_recent = 20132993  # Fri 20 Dec 17:52:50 EET 2024 
@@ -18,8 +23,8 @@ def main():
             with session.get(url, stream=True) as r:
                 if r.status_code != 200:
                     continue
-                file_name = file_name_template.format(idx=idx)
-                with open(file_name, "wb") as file:
+                filename = filename_template.format(idx=idx)
+                with open(output_dir / filename, "wb") as file:
                     for chunk in r.iter_content(chunk_size=128):
                         file.write(chunk)
 
